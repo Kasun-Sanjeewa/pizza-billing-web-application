4f50928136803cd9f8d8d01fdf537571c37f909d
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-01-07 02:47:01 +0530

    Ok

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -37,7 +37,7 @@ const SignIn = ({ onLogin }) => {
     return (
         <div className="signin-container">
             <div className="signin-left">
-                <h2>Sign in to Payshia POS System</h2>
+                <h2>Sign in to POS System</h2>
                 <form onSubmit={handleLogin}>
                     <div className="sign-form-group">
                         <input
